9aa4348b346d2d470453cded8be5eaa0a5c5fdd4
Frederic Migeon <frederic.migeon@irit.fr>
2022-11-01 09:09:37 +0100

    Terminated 1.a

---
+++ src/histoire/HistoireTP5.java
@@ -0,0 +1,9 @@
+package histoire;
+
+public class HistoireTP5 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -2,9 +2,14 @@ package personnages;
 
 public class Humain {
 
+	private static final int TAILLE_MEMOIRE = 50;
+
 	private String nom;
 	private String boissonFavorite;
 	private int qteArgent;
+
+	private Humain[] memoire = new Humain[TAILLE_MEMOIRE];
+	private int nbConnaissances = 0;
 
 	public Humain(String nom, String boissonFavorite, int qteArgent) {
 		this.nom = nom;
@@ -30,13 +35,37 @@ public class Humain {
 
 	public void acheter(String bien, int prix) {
 		if (qteArgent >= prix) {
-			parler("J'ai " + getQteArgent() + " sous en poche. " + "Je vais pouvoir m'offrir " + bien + " à " + prix
-					+ " sous.");
+			parler("J'ai " + getQteArgent() + " sous en poche. "
+					+ "Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
 			perdreArgent(prix);
 		} else {
-			parler("Je n'ai plus que " + qteArgent + " sous en poche." + "Je ne peux même pas m'offrir " + bien + " à "
-					+ prix + " sous.");
+			parler("Je n'ai plus que " + qteArgent + " sous en poche."
+					+ "Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
 
+		}
+	}
+
+	public void faireConnaissance(Humain nouvelleConnaissance) {
+		direBonjour();
+		nouvelleConnaissance.repondre(this);
+		memoriser(nouvelleConnaissance);
+	}
+
+	private void repondre(Humain autreHumain) {
+		direBonjour();
+		memoriser(autreHumain);
+	}
+
+	private void memoriser(Humain autreHumain) {
+		if (nbConnaissances == TAILLE_MEMOIRE) {
+			effacerLaPremiereConnaissanceEtDecalerVersLaGauche();
+		}
+		memoire[nbConnaissances++] = autreHumain;
+	}
+
+	private void effacerLaPremiereConnaissanceEtDecalerVersLaGauche() {
+		for (int i = 0; i < TAILLE_MEMOIRE - 1; i++) {
+			memoire[i] = memoire[i + 1];
 		}
 	}
 
